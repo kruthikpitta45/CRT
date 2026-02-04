d302b8dc9572d01f4582868ac79a72eb49bfbade
kruthikpitta45 <kruthikpitta45@gmail.com>
2026-02-04 10:20:40 +0530

    dgdh

---
+++ IV-SEM/Python/Practice/M01_Language_and_Programming_Basics/PS04_conditional_Statements.py
@@ -0,0 +1,19 @@
+
+p1 = "killing"
+for i in range(3):
+    p2 = input("enter password")
+    if p1==p2:
+        print("successful")
+        break
+else:
+    print("Locked")  
+
+
+
+
+    
+
+
+
+
+
