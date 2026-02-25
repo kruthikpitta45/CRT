96e63ba1de21153ab719dfca1f3ce45088ad0656
kruthikpitta45 <kruthikpitta45@gmail.com>
2026-02-25 15:26:12 +0530

    mes

--- IV-SEM/Python/Assignments/M01_Language_and_Programming_Basics/AST01/task.py
+++ IV-SEM/Python/Assignments/M01_Language_and_Programming_Basics/AST01/task.py
@@ -1,5 +1,12 @@
 def Ticket_Pricing(n: int) -> int:
-   pass
+    if n<5:
+        print("Free")
+    elif  n>=5 and n<=17:
+        print(10)   
+    elif n>=18 and n<=64:
+        print(20)
+    else:
+        print(15)
 
 
 if __name__ == '__main__':
